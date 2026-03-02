dcd409a3d10393dbb235dc79e6076830b8905c51
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-03-02 13:39:07 +0100

    installing json server and axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold">Poop Maxxxing</h1>
+      <h1 className="text-5xl font-bold">p**p</h1>
     </>
   );
 }
