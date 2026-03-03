df5c51acb2a542a828e00575be09fde9406cd51a
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-03-03 07:53:24 +0100

    rendering food lists

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,40 @@ function App() {
   const [users, setUsers] = useState<UserDto[]>([]);
 
   useEffect(() => {
-    setUsers(getAllUsers());
+    getAllUsers().then((response) => setUsers(response.data));
   }, []);
   return (
-    <>
-      <h1 className="text-5xl font-bold">p**p</h1>
-      <ul>
-        {users.map((u) => (
-          <li key={u.id}>
-            <h4>{u.name}</h4>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-5">p**p</h1>
+      {users.map((u) => (
+        <div className="mb-4 flex flex-col items-center" key={u.id}>
+          <h2 className="text-2xl font-bold mb-4">{u.name}</h2>
+          <div className="flex gap-16">
+            {" "}
+            <div className="mb-4">
+              <h4 className="text-xl font-bold">safe food list</h4>
+              <ul>
+                {u.safeFoodList.map((f) => (
+                  <li className="list-disc" key={f.id}>
+                    {f.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h4 className="text-xl font-bold">avoid food list</h4>
+              <ul>
+                {u.avoidFoodList.map((f) => (
+                  <li className="list-disc" key={f.id}>
+                    {f.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-const BASE_URL=import.meta.env.VITE_BACKEND_URL;
+const BASE_URL=import.meta.env.VITE_JSON_URL;
 
 export const getAllUsers = () => {
-  console.log(BASE_URL)
-  return axios.get(`${BASE_URL}/users`).then( (response) => response.data);
+  return axios.get(`${BASE_URL}/users`);
 }
