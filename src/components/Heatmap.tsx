07707a96fcfff03b03b2ff0552ce4f3141d6b690
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-04-13 13:33:55 +0200

    adding heatmap tooltip

--- src/components/Heatmap.tsx
+++ src/components/Heatmap.tsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
 import type { DailyLog } from "../types/DailyLogDto";
+import { useState } from "react";
+import type { PoopDTO } from "../types/PoopDto";
 
 type HeatmapDay = {
   date: string;
   score: number | null;
+  poop: PoopDTO | null;
 };
 
 const mapLogsByDate = (logs: DailyLog[]) => {
-  const map = new Map<string, number | null>();
+  const map = new Map<string, PoopDTO | null>();
 
   logs.forEach((log) => {
-    map.set(log.date, log.poopDTO?.score ?? null);
+    map.set(log.date, log.poopDTO ?? null);
   });
   return map;
 };
@@ -22,6 +26,7 @@ const generateYearDays = (year: number) => {
     days.push({
       date: date.toISOString().split("T")[0],
       score: null,
+      poop: null,
     });
 
     date.setDate(date.getDate() + 1);
@@ -34,10 +39,15 @@ const buildHeatmapData = (logs: DailyLog[], year: number) => {
   const logMap = mapLogsByDate(logs);
   const allDays = generateYearDays(year);
 
-  return allDays.map((day) => ({
-    ...day,
-    score: logMap.get(day.date) ?? null,
-  }));
+  return allDays.map((day) => {
+    const poop = logMap.get(day.date);
+
+    return {
+      ...day,
+      score: poop?.score ?? null,
+      poop,
+    };
+  });
 };
 
 const getColor = (score: number | null) => {
@@ -56,16 +66,24 @@ interface HeatmapProps {
 const Heatmap = ({ logs }: HeatmapProps) => {
   const currentYear = new Date().getFullYear();
   const days = buildHeatmapData(logs, currentYear);
+  const [hoveredDay, setHoveredDay] = useState<HeatmapDay | null>(null);
+  const [tooltipPos, setTooltipPos] = useState({ x: 0, y: 0 });
 
   return (
     <div className="heatmap">
       <div className="grid">
         {days.map((day) => (
-          <div
+          <Link
             key={day.date}
+            to={`/logs/${day.date}`}
             className="cell"
             style={{ backgroundColor: getColor(day.score) }}
-          ></div>
+            onMouseEnter={(e) => {
+              setHoveredDay(day);
+              setTooltipPos({ x: e.clientX, y: e.clientY });
+            }}
+            onMouseLeave={() => setHoveredDay(null)}
+          ></Link>
         ))}
       </div>
       <div className="flex items-center gap-1">
@@ -77,6 +95,35 @@ const Heatmap = ({ logs }: HeatmapProps) => {
         <div className="cell" style={{ backgroundColor: getColor(10) }}></div>
         <span>more</span>
       </div>
+      {hoveredDay && (
+        <div
+          className="tooltip"
+          style={{
+            top: tooltipPos.y - 30,
+            left: tooltipPos.x + 10,
+            position: "fixed",
+          }}
+        >
+          <h4>{hoveredDay.date}</h4>
+          <p>Score: {hoveredDay.score ?? 0}</p>
+          {hoveredDay.poop && (
+            <div className="flex gap-4">
+              <div className="">
+                <p>Color</p>
+                <p>{hoveredDay.poop.color}</p>
+              </div>
+              <div>
+                <p>Size</p>
+                <p>{hoveredDay.poop.size}</p>
+              </div>
+              <div>
+                <p>Consistency</p>
+                <p>{hoveredDay.poop.softness}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
